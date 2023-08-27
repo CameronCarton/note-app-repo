46751a1c2584e35f0e64a3baa4a60641fa5d048f
Cameron Carton <77831758+CameronCarton@users.noreply.github.com>
2023-08-27 14:57:20 +0100

    Edit and delete tasks
    added icon for edit and delete task,
    can now edit the text in a task and confirm edit with checkmark button

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ import '../src/App.css'
 
 function App() {
 
-  const themeColors = ["#0A1326", "#000000", "#FFFFFF", "#0C75FF"];
-  //const themeColors = ["#FFFFFF", "#DDDCE5", "#0C75FF", "#535789"];
+  const themeColors = ["#13223F", "#000000", "#FFFFFF", "#0C75FF"];
+  //const themeColors = ["#FFFFFF", "#DDDCE5", "#535789", "#0C75FF"];
 
   return (
     <>
--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,4 +1,9 @@
 import { useEffect, useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faXmark } from '@fortawesome/free-solid-svg-icons'
+import { faPen } from '@fortawesome/free-solid-svg-icons'
+import { faCheck } from '@fortawesome/free-solid-svg-icons'
+import React, { useRef } from "react";
 
 interface SectionProps {
     themeColors: string[];
@@ -7,19 +12,23 @@ interface SectionProps {
 function Section({themeColors}: SectionProps) {
 
     const toDoColor: string = themeColors[3];
-
+    const [editTaskText,setEditTaskText] = useState([0,0,0]);
     const [selectedTaskText,setSelectedTaskText] = useState("");
     const [tasks, setTasks] = useState([
         [],
         [],
         ["Task 1", "Task 2"],
     ]);
+    const editRef = useRef(null);
+
+
     
     // add a new task to a list
     const addTask = (list:number) => {
         const newTasks = [...tasks];
-        newTasks[list].push(".");
+        newTasks[list].push("");
         setTasks(newTasks);
+        setEditTaskText([1,list,tasks[list].length-1]);
     }
 
     // add task from existing list to another list
@@ -42,12 +51,16 @@ function Section({themeColors}: SectionProps) {
 
     // edit the text of a task
     const editTask = (list:number, index:number) => {
-        // edit the specified task text
-        const taskTextElement = document.querySelector(`.task-text[data-list="${list}"][data-index="${index}"]`) as HTMLElement;
-        if (taskTextElement) {
-            taskTextElement.contentEditable = 'true';
-            taskTextElement.focus();
-        }
+        setEditTaskText([1,list,index]);
+    }
+
+    // edit the text of a task
+    const confirmEditTask = (list:number, index:number, newEdit:string) => {
+        setEditTaskText([0,list,index]);
+
+        const newTasks = [...tasks];
+        newTasks[list][index] = newEdit;
+        setTasks(newTasks);
     }
 
     // select a task to move
@@ -58,6 +71,8 @@ function Section({themeColors}: SectionProps) {
             deleteTask(list,index);
         }
     }
+
+
 
     return (
         <>
@@ -83,7 +98,7 @@ function Section({themeColors}: SectionProps) {
                                         style={{
                                             borderColor: themeColors[0],
                                             backgroundColor: themeColors[0],
-                                            color: themeColors[3],
+                                            color: themeColors[2],
                                         }}
                                         onClick={() => selectTask(0,index,task)}
                                         >
@@ -111,7 +126,7 @@ function Section({themeColors}: SectionProps) {
                                         style={{
                                             borderColor: themeColors[0],
                                             backgroundColor: themeColors[0],
-                                            color: themeColors[3],
+                                            color: themeColors[2],
                                         }}
                                         onClick={() => selectTask(1,index,task)}
                                         >
@@ -143,14 +158,27 @@ function Section({themeColors}: SectionProps) {
                                         style={{
                                             borderColor: themeColors[0],
                                             backgroundColor: themeColors[0],
-                                            color: themeColors[3],
+                                            color: themeColors[2],
                                         }}
-                                        onClick={() => selectTask(2,index,task)}
                                         >
-                                            <div className="task-text">{task}</div>
+                                            {editTaskText[0]==1 && editTaskText[1]==2 && editTaskText[2]==index ?(
+                                                <div className="edit-task-text" contentEditable="true" ref={editRef}>{task}</div>
+                                            ) : (
+                                                <div className="task-text" onClick={() => selectTask(2,index,task)}>{task}</div>
+                                            )}
+
                                             <div style={{display:"flex"}}>
-                                                <div onClick={() => editTask(2,index)}>+</div>
-                                                <div onClick={() => deleteTask(2,index)}>-</div>
+                                                
+                                                {editTaskText[0]==1 && editTaskText[1]==2 && editTaskText[2]==index ?(
+                                                    <div className="edit-button" 
+                                                    onClick={() => confirmEditTask(2,index,editRef.current.innerText)}><FontAwesomeIcon icon={faCheck} /></div>
+                                                ) : (
+                                                    <div className="edit-button" 
+                                                    onClick={() => editTask(2,index)}><FontAwesomeIcon icon={faPen} size="xs"/></div>
+                                                )}
+
+                                                <div className="delete-button" 
+                                                onClick={() => deleteTask(2,index)}><FontAwesomeIcon icon={faXmark} /></div>
                                             </div>
                                         </div>
                                     ))}
